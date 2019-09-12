5d3715abf665fdadda69b7dce4e4acfa79aa67c7
Doruk Uluçay <doruk.ulucay@ingamegroup.com>
2019-09-12 11:14:04 +0300

    5000

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System;
+using System.Net;
 
 namespace Web
 {
@@ -19,6 +15,23 @@ namespace Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .UseStartup<Startup>()
+            .ConfigureKestrel((context, options) =>
+            {
+                options.Limits.MaxConcurrentConnections = 100;
+                options.Limits.MaxConcurrentUpgradedConnections = 100;
+                options.Limits.MaxRequestBodySize = 10 * 1024;
+                options.Limits.MinRequestBodyDataRate =
+                    new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                options.Limits.MinResponseDataRate =
+                    new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                options.Listen(IPAddress.Loopback, 5000);
+                //options.Listen(IPAddress.Loopback, 5001, listenOptions =>
+                //{
+                //    listenOptions.UseHttps("testCert.pfx", "testPassword");
+                //});
+                options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(2);
+                options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(1);
+            });
     }
 }
